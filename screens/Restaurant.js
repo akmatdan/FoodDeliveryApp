59ff4b0d681d094f04de1eeceac9221023607fe8
Dan Akmat <akmatdan@gmail.com>
2021-06-23 16:19:53 +0600

    Updated Restaurant page

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -85,9 +85,99 @@ const Restaurant = ({ route, navigation }) => {
     )
   }
 
+  function renderFoodInfo() {
+    return (
+      <Animated.ScrollView
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        showsHorizontalScrollIndicator={false}
+        // onScroll
+      >
+        {
+          restaurant?.menu.map((item, index) =>(
+            <View
+              key={`menu-${index}`}
+              style={{
+                alignItems: 'center'
+              }}
+            >
+              <View 
+              style={{
+                  height: SIZES.height * 0.35
+                }}
+              >
+                {/* Food Image */}
+                <Image 
+                  source={item.photo}
+                  resizeMode="cover"
+                  style={{
+                    width: SIZES.width,
+                    height: "100%"
+                  }}
+                />
+
+                {/* Quantity */}
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: -20,
+                    width: SIZES.width,
+                    height: 50,
+                    justifyContent: 'center',
+                    flexDirection: 'row'
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{
+                      width: 50,
+                      backgroundColor: 'white',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderTopLeftRadius: 25,
+                      borderBottomLeftRadius: 25
+                    }}
+                  >
+                    <Text style={{ ...FONTS.body1 }}>-</Text>
+                  </TouchableOpacity>
+
+                  <View
+                    style={{
+                      width: 50,
+                      backgroundColor: 'white',
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <Text style={{ ...FONTS.h2}}>5</Text>
+                  </View>
+
+                  <TouchableOpacity
+                    style={{
+                      width: 50,
+                      backgroundColor: 'white',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderTopRightRadius: 25,
+                      borderBottomRightRadius: 25,
+                    }}
+                  >
+                    <Text style={{ ...FONTS.body1 }}>+</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          ))
+        }
+      </Animated.ScrollView>
+    )
+  }
+
   return ( 
     <SafeAreaView style={styles.container}>
       {renderHeader()}
+      {renderFoodInfo()}
     </SafeAreaView>
   )
 }
